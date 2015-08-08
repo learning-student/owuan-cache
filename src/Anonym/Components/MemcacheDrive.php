27bdbaa7f5ebba45272053132d9c5e9a7dc8cf7b
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-09 00:49:48 +0300

    added methods

--- src/Anonym/Components/MemcacheDrive.php
+++ src/Anonym/Components/MemcacheDrive.php
@@ -17,4 +17,78 @@ namespace Anonym\Components\Cache;
 class MemcacheDriver extends AbstractDriver implements DriverInterface, DriverAdapterInterface
 {
 
+    /**
+     * Verinin değerini döndürür
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function get($name)
+    {
+
+    }
+
+    /**
+     * Veri ataması yapar
+     *
+     * @param string $name
+     * @param mixed $value
+     * @param int $time
+     * @return mixed
+     */
+    public function set($name, $value, $time = 3600)
+    {
+
+    }
+
+    /**
+     * @param string $name Değer ismi
+     * @return $this
+     */
+    public function delete($name)
+    {
+
+    }
+
+    /**
+     * Önbelleğe alınan tüm verileri siler
+     *
+     * @return mixed
+     */
+    public function flush()
+    {
+
+    }
+
+    /**
+     * Öyle bir değerin olup olmadığına bakar
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function exists($name)
+    {
+
+    }
+
+    /**
+     *
+     *
+     * @return bool
+     */
+    public function check()
+    {
+
+    }
+
+    /**
+     * Ayarları kullanır ve bazı başlangıç işlemlerini gerçekleştirir
+     *
+     * @param array $configs
+     * @return mixed
+     */
+    public function boot(array $configs = [])
+    {
+
+    }
 }
