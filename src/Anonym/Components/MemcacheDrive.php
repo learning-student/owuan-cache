5076372550b93405c3a4f7b6925f89e8878b6199
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-09 00:49:16 +0300

    added memcache driver

---
+++ src/Anonym/Components/MemcacheDrive.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
+
+
+namespace Anonym\Components\Cache;
+
+/**
+ * Class MemcacheDriver
+ * @package Anonym\Components\Cache
+ */
+class MemcacheDriver extends AbstractDriver implements DriverInterface, DriverAdapterInterface
+{
+
+}
