12d549920767f024ef41a63f2d0c773f744c1e6f
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-09 18:01:01 +0300

    added some methods

--- src/Anonym/Components/PredisCacheDriver.php
+++ src/Anonym/Components/PredisCacheDriver.php
@@ -14,7 +14,81 @@ namespace Anonym\Components\Cache;
  * Class PredisCacheDriver
  * @package Anonym\Components\Cache
  */
-class PredisCacheDriver
+class PredisCacheDriver implements DriverAdapterInterface, DriverInterface
 {
 
+    /**
+     * Verinin değerini döndürür
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function get($name)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * Veri ataması yapar
+     *
+     * @param string $name
+     * @param mixed $value
+     * @param int $time
+     * @return mixed
+     */
+    public function set($name, $value, $time = 3600)
+    {
+        // TODO: Implement set() method.
+    }
+
+    /**
+     * @param string $name Değer ismi
+     * @return mixed
+     */
+    public function delete($name)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    /**
+     * Önbelleğe alınan tüm verileri siler
+     *
+     * @return mixed
+     */
+    public function flush()
+    {
+        // TODO: Implement flush() method.
+    }
+
+    /**
+     * Öyle bir değerin olup olmadığına bakar
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function exists($name)
+    {
+        // TODO: Implement exists() method.
+    }
+
+    /**
+     *
+     *
+     * @return bool
+     */
+    public function check()
+    {
+        // TODO: Implement check() method.
+    }
+
+    /**
+     * Ayarları kullanır ve bazı başlangıç işlemlerini gerçekleştirir
+     *
+     * @param array $configs
+     * @return mixed
+     */
+    public function boot(array $configs = [])
+    {
+        // TODO: Implement boot() method.
+    }
 }
