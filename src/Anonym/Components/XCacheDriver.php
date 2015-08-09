1e22c518d5cbf0173bfa93af8e0b670d1bdb1873
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-09 17:15:58 +0300

    updated xcache driver

--- src/Anonym/Components/XCacheDriver.php
+++ src/Anonym/Components/XCacheDriver.php
@@ -14,7 +14,91 @@ namespace Anonym\Components\Cache;
  * Class XCacheDriver
  * @package Anonym\Components\Cache
  */
-class XCacheDriver
+class XCacheDriver implements DriverInterface, DriverAdapterInterface
 {
 
+    /**
+     * Verinin değerini döndürür
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function get($name)
+    {
+        return xcache_get($name);
+    }
+
+    /**
+     * Veri ataması yapar
+     *
+     * @param string $name
+     * @param mixed $value
+     * @param int $time
+     * @return mixed
+     */
+    public function set($name, $value, $time = 3600)
+    {
+         return xcache_set($name, $value, $time);
+    }
+
+    /**
+     * @param string $name Değer ismi
+     * @return mixed
+     */
+    public function delete($name)
+    {
+        return xcache_unset($name);
+    }
+
+    /**
+     * Önbelleğe alınan tüm verileri siler
+     *
+     * @return mixed
+     */
+    public function flush()
+    {
+        for($i = 0; $i < xcache_count(XC_TYPE_PHP); $i++){
+            // XCache PHP cache temizle.
+            xcache_clear_cache(XC_TYPE_PHP, $i);
+        }
+
+        for($i = 0; $i < xcache_count(XC_TYPE_VAR); $i++){
+            // XCache degisken cache temizle.
+            xcache_clear_cache(XC_TYPE_VAR, $i);
+        }
+
+        return true;
+    }
+
+    /**
+     * Öyle bir değerin olup olmadığına bakar
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function exists($name)
+    {
+        return xcache_isset($name);
+    }
+
+    /**
+     *Xcache sürücüsünün olup olmadığını kontrol ediyoruz
+     *
+     * @return bool
+     */
+    public function check()
+    {
+         return function_exists('xcache_set');
+    }
+
+    /**
+     * Ayarları kullanır ve bazı başlangıç işlemlerini gerçekleştirir
+     *
+     * @param array $configs
+     * @return mixed
+     */
+    public function boot(array $configs = [])
+    {
+        // we not have do somethink
+    }
 }
