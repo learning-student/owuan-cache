1ba8886be1835a5d5e7c3fcd69558c61b0767761
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-09 17:45:47 +0300

    updated zend data cache

--- src/Anonym/Components/ZendDataCache.php
+++ src/Anonym/Components/ZendDataCache.php
@@ -17,4 +17,78 @@ namespace Anonym\Components\Cache;
 class ZendDataCache implements DriverAdapterInterface, DriverInterface
 {
 
+    /**
+     * Verinin değerini döndürür
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function get($name)
+    {
+         return zend_shm_cache_fetch($name);
+    }
+
+    /**
+     * Veri ataması yapar
+     *
+     * @param string $name
+     * @param mixed $value
+     * @param int $time
+     * @return mixed
+     */
+    public function set($name, $value, $time = 3600)
+    {
+        return zend_shm_cache_store($name, $value, $time);
+    }
+
+    /**
+     * @param string $name Değer ismi
+     * @return mixed
+     */
+    public function delete($name)
+    {
+        return zend_shm_cache_delete($name);
+    }
+
+    /**
+     * Önbelleğe alınan tüm verileri siler
+     *
+     * @return mixed
+     */
+    public function flush()
+    {
+        return zend_shm_cache_clear();
+    }
+
+    /**
+     * Öyle bir değerin olup olmadığına bakar
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function exists($name)
+    {
+        return (false !== $this->get($name));
+    }
+
+    /**
+     * Zend data cache in kurulu olup olmadığını kontrol eder
+     *
+     * @return bool
+     */
+    public function check()
+    {
+        return function_exists('zend_shm_cache_fetch');
+    }
+
+    /**
+     * Ayarları kullanır ve bazı başlangıç işlemlerini gerçekleştirir
+     *
+     * @param array $configs
+     * @return mixed
+     */
+    public function boot(array $configs = [])
+    {
+        //
+    }
 }
