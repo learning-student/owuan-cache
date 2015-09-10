2ce2e5ae47101ecbc86ca0bbf0dac8c0ed67395b
anonymframework <serif.saglam119@outlook.com>
2015-09-10 21:02:16 +0300

    made some updates

--- CacheServiceProvide.php
+++ CacheServiceProvide.php
@@ -8,9 +8,23 @@
  */
 
 namespace Anonym\Components\Cache;
-
-
-class CacheServiceProvider
+use Anonym\Bootstrap\ServiceProvider;
+use Anonym\Facades\Config;
+/**
+ * service provider of cache component
+ *
+ * Class CacheServiceProvider
+ * @package Anonym\Components\Cache
+ */
+class CacheServiceProvider extends ServiceProvider
 {
+
+    public function register(){
+
+        $this->singleton('cache', function(){
+           $configs = Config::get();
+        });
+
+    }
 
 }
